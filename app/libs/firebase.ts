f69a2ae923a8393479d129a4299ac5e32bccb5ca
Trygve <57544728+TrygveDev@users.noreply.github.com>
2023-05-02 17:22:55 +0200

    v2.2 env 2

--- app/libs/firebase.ts
+++ app/libs/firebase.ts
@@ -9,16 +9,7 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-	apiKey: process.env.FIREBASE_apiKey,
-	authDomain: process.env.FIREBASE_authDomain,
-	databaseURL: process.env.FIREBASE_databaseURL,
-	projectId: process.env.FIREBASE_projectId,
-	storageBucket: process.env.FIREBASE_storageBucket,
-	messagingSenderId: process.env.FIREBASE_messagingSenderId,
-	appId: process.env.FIREBASE_appId,
-	measurementId: process.env.FIREBASE_measurementId,
-};
+import firebaseConfig from "./firebaseConfig";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
