d54734ab4091f04fc2d0c28165d071efe43e1038
Trygve <57544728+TrygveDev@users.noreply.github.com>
2023-05-15 18:00:47 +0200

    v4.4 ENV

--- app/libs/firebase.ts
+++ app/libs/firebase.ts
@@ -1,5 +1,3 @@
-"use client";
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -12,15 +10,16 @@ import { getDatabase } from "firebase/database";
 
 // Initialize Firebase
 const app = initializeApp({
-	apiKey: process.env.apiKey,
-	authDomain: process.env.authDomain,
-	databaseURL: process.env.databaseURL,
-	projectId: process.env.projectId,
-	storageBucket: process.env.storageBucket,
-	messagingSenderId: process.env.messagingSenderId,
-	appId: process.env.appId,
-	measurementId: process.env.measurementId,
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 });
+console.log(process.env);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
